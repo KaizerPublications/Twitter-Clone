5fb24d5f01a29a1cc1505862e15a5e68b1bf593c
Dennis Kuria <kaizer@disroot.org>
2024-12-19 18:39:09 +0300

    3 - Auth handled and routes protected in client

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -66,21 +66,21 @@ export const login = async (req, res) => {
     const user = await User.findOne({ username });
     const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
 
-    if (!user || isPasswordCorrect) {
+    if (!user || !isPasswordCorrect) {
       return res.status(400).json({ error: "Invalid username or password" })
     }
 
     generateTokenAndSetCookie(user._id, res);
 
     res.status(200).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      email: newUser.email,
-      followers: newUser.followers,
-      following: newUser.following,
-      profileImg: newUser.profileImg,
-      coverImg: newUser.coverImg,
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      email: user.email,
+      followers: user.followers,
+      following: user.following,
+      profileImg: user.profileImg,
+      coverImg: user.coverImg,
     })
 
   } catch (error) {
